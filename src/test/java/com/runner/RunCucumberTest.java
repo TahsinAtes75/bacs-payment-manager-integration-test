61b962f2aa735aac341014e876a71b900ad8b398
zeynepusta <z.usta@hymnai.com>
2020-08-04 14:04:26 +0100

    Initial Commit

---
+++ src/test/java/com/runner/RunCucumberTest.java
@@ -0,0 +1,32 @@
+package com.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+
+        plugin = {
+                "pretty",
+                "json:target/cucumber.json",
+                "html:target/default-cucumber-reports"
+        },
+
+        tags = {"@runner"},
+
+        features = {"src/test/resources/features"
+        },
+
+        glue = {"com/hymnai/backend/steps"},
+
+        dryRun = false
+
+)
+
+
+
+public class RunCucumberTest {
+
+}
